3f05f3da89ffddac0ed61a190015b7f8431ae047
csj1972 <csj9630@gmail.com~>
2025-10-14 20:51:42 +0900

    first commit

---
+++ src/Package1/MainScreen.java
@@ -0,0 +1,12 @@
+package Package1;
+
+public class MainScreen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이 프로젝트는 테스트용입니다.");
+		System.out.println("시험 삼아 기능 구현 같은 걸 여기서 해볼 예정.");
+		System.out.println("시험 삼아 기능 구현 같은 걸 여기서 해볼 예정.");
+	}
+
+}
