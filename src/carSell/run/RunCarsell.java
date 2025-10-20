a3a30f0f1c467d9a6cdde15dbc09f1a9957923f0
csjSist <csj9630@gmail.com>
2025-10-20 11:59:19 +0900

    일단 내정보메뉴 디자인 적당히 만듬.

--- src/carSell/run/RunCarsell.java
+++ src/carSell/run/RunCarsell.java
@@ -1,10 +1,12 @@
 package carSell.run;
 
+import carSell.design.UserMenuDesign;
+
 public class RunCarsell {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new UserMenuDesign();
 	}
 
 }
