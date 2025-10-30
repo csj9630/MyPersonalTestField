1dd50c643ed4929f73b89d7fd61ab7937b4dca82
csj1972 <csj9630@gmail.com~>
2025-10-30 21:28:15 +0900

    blob테스트를 위해 day1014 복사해옴

---
+++ src/day1014_BlobTest/RunWork1013.java
@@ -0,0 +1,14 @@
+package day1014_BlobTest;
+
+import kr.co.sist.util.img.ImageResize;
+
+public class RunWork1013 {
+
+	public static void main(String[] args) {
+		
+		new Work1013Design();
+//		ImageResize.resizeImage("C:/dev/workspace/jdbc_prj/src/day1014/images/img_1.png", 200, 170);
+
+	}
+
+}
